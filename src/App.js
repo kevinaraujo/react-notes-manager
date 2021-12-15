38117c0ddfc18364d220ef21f447f8d294e2c8be
Kevin Araújo <kfa_34@hotmail.com>
2021-12-15 18:37:26 -0300

    Added method in app element to save the informations.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import NotesList from './components/NotesList'
 import RegisterForm from './components/RegisterForm'
 import './assets/App.css'
 class App extends Component {
+
+  createNote(title, text) {
+
+    console.log('ok')
+    console.log(title, text);
+  }
+
   render() {
     return (
       <section className="content">
-        <RegisterForm/>
+        <RegisterForm createNote={ this.createNote } />
         <NotesList/>
       </section>
     );
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -3,31 +3,48 @@ import './style.css'
 
 class RegisterForm extends Component {
 
-    constructor() {
+    constructor(props) {
         super()
 
         this.title = ''
+        this.text = ''
     }
 
-    handlerTitleChance(e) {
-        this.title = e.target.value
-        console.log(this.title)
+    _handlerTitleChange(e) {
+        this.title = e.target.value        
+    }
+
+    _handleTextChange(e) {
+        this.text = e.target.value
+    }
+
+    _createNote(e) {
+        e.preventDefault()
+        e.stopPropagation()
+
+        this.props.createNote(this.title, this.text)
+        console.log('osk')
+        console.log(this.title, this.text);
     }
 
     render() {
         return (
-            <form className="register-form"> 
+            <form 
+                className="register-form"
+                onSubmit={ this._createNote.bind(this) }
+            > 
                 <input 
                     type="text" 
                     placeholder="Title"
                     className="register-form-input"
-                    onChange={ this.handlerTitleChance.bind(this) }
+                    onChange={ this._handlerTitleChange.bind(this) }
                     
                 />
                 <textarea 
                     rows={ 15 }
                     placeholder="Type your note..."
                     className="register-form-input"
+                    onChange={ this._handleTextChange.bind(this) }
                 />
 
                 <button className="register-form-input register-form-submit">
